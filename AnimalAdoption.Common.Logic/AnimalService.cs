ce14c62adbc61da516fa6c37f4883b089b90aa01
Greg Walker <Greg.Walker@ccc.govt.nz>
2020-09-03 11:40:38 +1200

    new photo

--- AnimalAdoption.Common.Logic/AnimalService.cs
+++ AnimalAdoption.Common.Logic/AnimalService.cs
@@ -6,6 +6,7 @@
             new Animal { Id = 1, Name = "big rock", Age = 50, Description = "Soft natured" },
             new Animal { Id = 2, Name = "small rock", Age = 50, Description = "Under a lot of pressure" },
             new Animal { Id = 3, Name = "diamond", Age = 50, Description = "Shiny and glasslike" },
+            new Animal { Id = 4, Name = "crazzy guy", Age = 2, Description = "bonkers"}
         };
     }
 }
